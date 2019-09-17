be51aaf76bd1ac61e0bd16ea68c383e707a587dc
Swamy Kumar <swamy@Swamys-MacBook-Air.local>
2019-09-17 17:51:57 +0530

    changes suggested by D 17-09-2019

--- src/app/pages/team-member-info/team-member-info.module.ts
+++ src/app/pages/team-member-info/team-member-info.module.ts
@@ -6,6 +6,8 @@ import { Routes, RouterModule } from '@angular/router';
 import { IonicModule } from '@ionic/angular';
 
 import { TeamMemberInfoPage } from './team-member-info.page';
+import { LineGraphComponent } from '../../line-graph/line-graph.component';
+import { BarGraphHtmlComponent } from 'src/app/bar-graph-html/bar-graph-html.component';
 
 const routes: Routes = [
   {
@@ -21,6 +23,6 @@ const routes: Routes = [
     IonicModule,
     RouterModule.forChild(routes)
   ],
-  declarations: [TeamMemberInfoPage]
+  declarations: [TeamMemberInfoPage, LineGraphComponent, BarGraphHtmlComponent]
 })
 export class TeamMemberInfoPageModule {}
