bd5561cf7c0aeccf60ef587cf11691c73e42669b
Swamy Kumar <swamy@Swamys-MacBook-Air.local>
2019-09-20 11:07:09 +0530

    logout and routing changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'isd-login', pathMatch: 'full' },
+  // { path: '', redirectTo: 'isd-login', pathMatch: 'full' },
   { path: 'isd-login', loadChildren: './pages/isd-login/isd-login.module#IsdLoginPageModule' },
   { path: 'team-member-info', loadChildren: './pages/team-member-info/team-member-info.module#TeamMemberInfoPageModule' },
   { path: 'adminpage', loadChildren: './pages/adminpage/adminpage.module#AdminpagePageModule' },
